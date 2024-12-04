010ac7731f5828d8add52d153ca94c318b06e794
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2024-12-04 16:07:28 +0100

    "Finish part 2 of day 4"

--- src/main/java/org/day4/Main.java
+++ src/main/java/org/day4/Main.java
@@ -17,9 +17,17 @@ public class Main {
         return Arrays.stream(input.split("\n")).map(String::toCharArray).toArray(char[][]::new);
     }
 
-    private static boolean readXmas(final char[][] map, int x, int y, final int xDir, final int yDir) {
+    private static boolean readString(
+            final char[][] map,
+            final String str,
+            int x,
+            int y,
+            final int xDir,
+            final int yDir
+    ) {
+        final String reverse = new StringBuilder(str).reverse().toString();
         final var stack = new Stack<Character>();
-        stack.addAll(List.of('S', 'A', 'M', 'X'));
+        stack.addAll(List.of(reverse.chars().mapToObj(c -> (char) c).toArray(Character[]::new)));
         while (!stack.isEmpty() && 0 <= y && y < map.length && 0 <= x && x < map[y].length) {
             final char next = stack.pop();
             if (map[y][x] != next) {
@@ -46,8 +54,32 @@ public class Main {
         for (final int[] dir : dirs) {
             for (int y = 0; y < map.length; y++) {
                 for (int x = 0; x < map[y].length; x++) {
-                    if (Main.readXmas(map, x, y, dir[0], dir[1])) {
+                    if (Main.readString(map, "XMAS", x, y, dir[0], dir[1])) {
                         count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    private static int countCrossMas(final char[][] map) {
+        int count = 0;
+        final int[][] countMap = new int[map.length][map[0].length];
+        final int[][] dirs = {
+                {1, 1},
+                {1, -1},
+                {-1, 1},
+                {-1, -1}
+        };
+        for (final int[] dir : dirs) {
+            for (int y = 0; y < map.length; y++) {
+                for (int x = 0; x < map[y].length; x++) {
+                    if (Main.readString(map, "MAS", x, y, dir[0], dir[1])) {
+                        countMap[y + dir[1]][x + dir[0]]++;
+                        if (2 == countMap[y + dir[1]][x + dir[0]]) {
+                            count++;
+                        }
                     }
                 }
             }
@@ -60,5 +92,7 @@ public class Main {
         final char[][] map = Main.parseInput(input);
         final int result1 = Main.countXmas(map);
         System.out.println("Part 1: " + result1);
+        final int result2 = Main.countCrossMas(map);
+        System.out.println("Part 2: " + result2);
     }
 }
