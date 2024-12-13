72b33319f0e41704dac720680f468290293aa879
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2024-12-13 22:49:17 +0100

    Add day 14 template

---
+++ src/main/java/org/day14/Main.java
@@ -0,0 +1,16 @@
+package org.day14;
+
+import org.common.Toolbox;
+
+public class Main {
+
+  private static final String inputFile = "input_day14.txt";
+
+  private Main() {
+    super();
+  }
+
+  public static void main(final String[] args) {
+    final String input = Toolbox.getInput(inputFile);
+  }
+}
