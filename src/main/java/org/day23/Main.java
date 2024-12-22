b3423c0d9127ca0236994866f75b755fbb48c3d5
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2024-12-22 14:42:28 +0100

    Add day 23 template

---
+++ src/main/java/org/day23/Main.java
@@ -0,0 +1,17 @@
+package org.day23;
+
+import org.common.Toolbox;
+
+public class Main {
+
+  private static final String inputFile = "input_day23.txt";
+
+  private Main() {
+    super();
+  }
+
+  public static void main(final String[] args) {
+    final String input = Toolbox.getInput(inputFile);
+    System.out.println(input);
+  }
+}
